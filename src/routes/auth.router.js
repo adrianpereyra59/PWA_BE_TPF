1ab569f7b6e4504af5235bfd4f3cebe55352e7e1
adrianpereyra59 <adrianpereyra59@gmail.com>
2025-11-27 20:59:02 -0300

    V1.4

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,10 +1,13 @@
-import express from 'express';
-import AuthController from '../controllers/auth.controller.js';
+import express from "express";
+import AuthController from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.post('/register', AuthController.register);
-router.post('/login', AuthController.login);
-router.get('/verify-email/:verification_token', AuthController.verifyEmail);
+router.post("/register", AuthController.register);
+router.post("/login", AuthController.login);
+router.get("/verify-email/:verification_token", AuthController.verifyEmail);
+
+router.post("/forgot-password", AuthController.forgotPassword);
+router.post("/reset-password", AuthController.resetPassword);
 
 export default router;
